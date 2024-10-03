8e9c11932f4a219132e68a08c2a78714c0c9a193
Bruna de Oliveira <bruna.soliveira@icloud.com>
2024-10-03 11:13:56 +0100

    Step 4 Models Associations and Validations Added - Awesome Garage

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,8 @@
 class Car < ApplicationRecord
   belongs_to :owner
+  has_many :reviews, dependent: :destroy
+  has_many :favourites, dependent: :destroy
+
+  validates :brand, :model, :fuel, presence: true
+  validates :year, presence: true, numericality: { only_integer: true }
 end
