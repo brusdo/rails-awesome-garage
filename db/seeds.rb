fe6777e366bb87e14896764e37ed56a01499eda4
Bruna de Oliveira <bruna.soliveira@icloud.com>
2024-10-03 14:47:48 +0100

    Step 5 File Seeded - Awesome Garage

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# The other matters: destroy children to then destroy the parent
+Review.destroy_all
+Favourite.destroy_all
+Car.destroy_all
+Owner.destroy_all
+
+# Start creating the parent
+owner_1 = Owner.create!(nickname: "ElonM")
+owner_2 = Owner.create!(nickname: "RonW")
+owner_3 = Owner.create!(nickname: "SenaA")
+
+car_1 = Car.create!(brand: "Tesla", model: "Model 3", year: 2024, fuel: "Unleaded petrol", owner: owner_1)
+car_2 = Car.create!(brand: "Nissan", model: "Rogue", year: 2017, fuel: "Unleaded petrol", owner: owner_2)
+car_3 = Car.create!(brand: "BMW", model: "X3", year: 2023, fuel: "Diesel", owner: owner_3)
+
+Review.create!(comment: "Nice car!", rating: 5, car: car_1)
+Review.create!(comment: "It is okay-ish", rating: 2, car: car_2)
+Review.create!(comment: "It is fast!", rating: 4, car: car_3)
+
+puts "Seed created! #{Owner.all.count} owners created and #{Car.all.count} created"
